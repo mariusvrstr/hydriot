96c4308cf8ae69692286d14a1625612be3a0996a
Marius <marius.vrstr@gmail.com>
2021-02-20 08:39:29 +0200

    Made sample code switch between low/high voltage relays

--- Raspberry Pi/Relay Switch (Subaligu)/relay_example.py
+++ Raspberry Pi/Relay Switch (Subaligu)/relay_example.py
@@ -4,7 +4,7 @@ import time
 # IMPORTANT: In this example I had to connect my relay switch to the 3.3v power (Quite common) and swap LOW/HIG
 
 relay_pin_pos = 16 # Which PIN is used on the Pi
-is_high = False
+is_low_volt_relay = True # Use this when connected to 3.3V source (If it does switch off use this and switch to 3.3V)
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -12,11 +12,11 @@ GPIO.setup(relay_pin_pos, GPIO.OUT) # GPIO Assign mode
 
 try:
     while True:
-        GPIO.output(relay_pin_pos, GPIO.HIGH) # OFF
+        GPIO.output(relay_pin_pos, GPIO.HIGH if is_low_volt_relay else GPIO.LOW) # OFF
         print("Switched off")
         time.sleep(5)
 
-        GPIO.output(relay_pin_pos, GPIO.LOW) # ON
+        GPIO.output(relay_pin_pos, GPIO.LOW if is_low_volt_relay else GPIO.HIGH) # ON
         print("Switched on")
         time.sleep(5)      
 
