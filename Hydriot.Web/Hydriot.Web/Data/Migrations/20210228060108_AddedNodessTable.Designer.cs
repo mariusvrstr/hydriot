b6dfe48cf199dcab9e907a577b4e03e06b9e127e
Marius Vorster <marius.vrstr@gmail.com>
2021-02-28 08:13:15 +0200

    Added migrations and link to nodes page

--- Hydriot.Web/Hydriot.Web/Data/Migrations/20210228060108_AddedNodessTable.Designer.cs
+++ Hydriot.Web/Hydriot.Web/Data/Migrations/20210228060108_AddedNodessTable.Designer.cs
@@ -4,14 +4,16 @@ using Hydriot.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hydriot.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210228060108_AddedNodessTable")]
+    partial class AddedNodessTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Hydriot.Web/Hydriot.Web/Data/Migrations/20210228060108_AddedNodessTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hydriot.Web.Data.Migrations
+{
+    public partial class AddedNodessTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Node",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeviceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeviceId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegisteredDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastConnected = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Node", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Node");
+        }
+    }
+}
