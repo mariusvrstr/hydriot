e9ed7655dd151cc13070ea7d04861f382158632a
Marius Vorster <marius.vrstr@gmail.com>
2021-03-13 22:39:19 +0200

    Added basic API for sensor updates and Sensor Data Caching

---
+++ Hydriot.Web/Hydriot.Web/Controller/NodeController.cs
@@ -0,0 +1,91 @@
+﻿using Hydriot.Web.Data;
+using Hydriot.Web.Data.Entities;
+using Hydriot.Web.Data.Repositories;
+using Hydriot.Web.Models;
+using Hydriot.Web.Monitor;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hydriot.Web.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NodeController : ControllerBase
+    {
+
+        private readonly NodeRepository _nodesRepo;
+
+        public NodeController(ApplicationDbContext context)
+        {
+            _nodesRepo = new NodeRepository(context);
+        }
+
+
+        [HttpGet("{nodeId}")]
+        public ActionResult<Node> GetNode(Guid nodeId)
+        {
+            var node = _nodesRepo.GetById(nodeId);
+
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            return node;
+        }
+
+        [HttpGet("GetNodeSensorData/{nodeId}")]
+        public ActionResult<NodeData> GetNodeSensorData(Guid nodeId)
+        {
+            var node = _nodesRepo.GetById(nodeId);
+
+            if (node == null)
+            {
+                return NotFound();
+            }
+
+            var latestNodeData = NodeCache.Instance.GetLatestNodeSensorReadings(nodeId);
+
+            return latestNodeData;
+        }
+
+        [HttpPost]
+        public ActionResult<Guid> RegisterNewNode([FromBody] string name)
+        {
+            // Get the underlying account
+
+            // Create a new node to the database for this account
+
+            // Add to the in-memory cache
+
+            // Return the ID
+
+            throw new NotImplementedException();
+        }
+
+
+
+        [HttpPut("UpdateNodeSensors/{nodeId}")]
+        public ActionResult<NodeData> UpdateNodeSensors(Guid nodeId, [FromBody] IEnumerable<SensorData> sensors)
+        {
+            var foundNode = _nodesRepo.GetById(nodeId);
+
+            // TODO: Check that node is part of curren tuser account
+
+            if (foundNode == null)
+            {
+                throw new InvalidProgramException($"Cannot update an node [{nodeId}] that does not yet exist. First register the node.");
+            }
+
+            var latestNodeData = new NodeData(nodeId, sensors);
+            NodeCache.Instance.UpdateSensorReadings(latestNodeData);
+
+            var mergedNodeData = NodeCache.Instance.GetLatestNodeSensorReadings(nodeId);
+
+            return mergedNodeData;
+        }
+    }
+}
---
+++ Hydriot.Web/Hydriot.Web/Models/NodeData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hydriot.Web.Models
+{
+    public class NodeData
+    {
+        public NodeData(Guid id, IEnumerable<SensorData> sensors)
+        {
+            Id = id;
+            Sensors = sensors;
+        }
+
+        public Guid Id { get; set; }
+
+        public IEnumerable<SensorData> Sensors { get; set; } = new List<SensorData>();
+
+
+        public void UpdateSensorsData(IEnumerable<SensorData> latestData)
+        {
+
+        }
+
+    }
+}
