852f4fcf70f41ec49b706793bb150d99a50f32e2
Marius <marius.vrstr@gmail.com>
2021-07-03 14:14:55 +0200

    Added back the actual relay activity, button press

--- Hydriot.PiAgent/tasks/nutrient_dose_task.py
+++ Hydriot.PiAgent/tasks/nutrient_dose_task.py
@@ -1,14 +1,17 @@
-import time
+import asyncio
 
 from tasks.contracts.base_task import BaseTask
+from utilities.dependency_injection import Container
+from settings.trigger_config import TriggerConfig
+from utilities.dependency_injection import Container
 
 class NutrientDoseTask(BaseTask):
 
     def run_custom(self):
         print(f"starting example task [NutrientDoseTask]")
-        
-        for i in range(5, 0, -1):
-            print(f"Tic [{i}]")
-            time.sleep(1)
 
-    
+        nutrient_trigger = Container().nutrient_relay_factory()
+        ## nutrient_trigger.set_tds_sensor_summary(tds_summary)
+        dose_duration_seconds = TriggerConfig().get_tds_dose_time_seconds()
+
+        asyncio.run(nutrient_trigger.dose(dose_duration_seconds))
