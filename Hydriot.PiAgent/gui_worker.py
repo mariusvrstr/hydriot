777ccd7fc17f399bb4cf71e99a98d0197b8d2189
Marius <marius.vrstr@gmail.com>
2021-07-03 14:15:18 +0200

    Added relay activity from UX button

--- Hydriot.PiAgent/gui_worker.py
+++ Hydriot.PiAgent/gui_worker.py
@@ -1,9 +1,6 @@
 from time import sleep
 
 from PyQt5.QtCore import QThread
-from utilities.dependency_injection import Container
-from settings.trigger_config import TriggerConfig
-from utilities.dependency_injection import Container
 from utilities.thread_wrapper import ThreadWrapper
 from tasks.nutrient_dose_task import NutrientDoseTask
 from tasks.ph_down_dose_task import PhDownTask
@@ -26,12 +23,6 @@ class GuiWorker:
         wrapper = ThreadWrapper(self.task_manager, task, button, label)
         wrapper.run_task(self._ph_thread)
 
-        ## ph_down_trigger = Container().ph_down_relay_factory()
-        ## ph_down_trigger.set_ph_down_sensor_summary(ph_down_summary)
-        ## dose_duration_seconds = TriggerConfig().get_ph_down_dose_time_seconds()
-
-        ## asyncio.run(ph_down_trigger.dose(dose_duration_seconds))
-
     def  action_nutrient_dose(self, button, label):
         task = NutrientDoseTask()
 
@@ -39,11 +30,6 @@ class GuiWorker:
             self._nutrient_thread = QThread()
 
         wrapper = ThreadWrapper(self.task_manager, task, button, label)
-        wrapper.run_task(self._nutrient_thread)
-        
-        ## nutrient_trigger = Container().nutrient_relay_factory()
-        ## nutrient_trigger.set_tds_sensor_summary(tds_summary)
-        ## dose_duration_seconds = TriggerConfig().get_tds_dose_time_seconds()
+        wrapper.run_task(self._nutrient_thread)       
 
-        ## asyncio.run(nutrient_trigger.dose(dose_duration_seconds))
 
--- Hydriot.PiAgent/tasks/ph_down_dose_task.py
+++ Hydriot.PiAgent/tasks/ph_down_dose_task.py
@@ -1,12 +1,17 @@
-import time
+import asyncio
 
 from tasks.contracts.base_task import BaseTask
+from utilities.dependency_injection import Container
+from settings.trigger_config import TriggerConfig
+from utilities.dependency_injection import Container
 
 class PhDownTask(BaseTask):
 
     def run_custom(self):
         print(f"starting example task [PhDownTask]")
 
-        for i in range(5, 0, -1):
-            print(f"Tic [{i}]")
-            time.sleep(1)
+        ph_down_trigger = Container().ph_down_relay_factory()
+        ## ph_down_trigger.set_ph_down_sensor_summary(ph_down_summary)
+        dose_duration_seconds = TriggerConfig().get_ph_down_dose_time_seconds()
+
+        asyncio.run(ph_down_trigger.dose(dose_duration_seconds))
